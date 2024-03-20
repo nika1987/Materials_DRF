964703e119d7b140b0358f357518c00874a59146
veronika <@Nika_Zolotova>
2024-03-20 15:11:56 +0400

    + django_celary_beat

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'django_filters'
+    'django_filters',
     'rest_framework_simplejwt',
     'drf_yasg',
     'django_celery_beat',
